3ffdd9e27d042c2ed2f66061c8e9f3f9492a14af
lilian1016 <bone861016@gmail.com>
2017-11-07 15:34:46 +0800

    prime

---
+++ src/com/lilian/test.java
@@ -0,0 +1,23 @@
+package com.lilian;
+
+public class test {
+	public static void main(String[]args){
+		for(int i = 1 ; i <= 9 ; i++){
+	    	 for(int j = 2 ; j <=5 ; j++){
+    	System.out.print( j + " x "+ i +" = " + (j*i) +"\t");
+	    	 }
+	    	 System.out.println();
+ /*      for(int i = 1 ; i <= 9 ; i++){
+    	 for(int j = 2 ; j <=5 ; j++){
+     System.out.print( j + " x "+ i +" = ");
+     if((j * i)<10){
+    	 System.out.print(" ");
+     }
+     System.out.print((j * i));
+     System.out.print("   ");
+     }
+    	 System.out.println();
+   }*/
+}
+}
+}
