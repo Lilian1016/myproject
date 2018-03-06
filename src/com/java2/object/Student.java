6ceac1aab4dbe6427552de011188e45f087419db
lilian1016 <bone861016@gmail.com>
2018-03-06 15:56:26 +0800

    class

---
+++ src/com/java2/object/Student.java
@@ -0,0 +1,14 @@
+package com.java2.object;
+
+public class Student {
+	String name;
+	int score;
+	String id;
+	  
+	public Student(String name,String id,int score){
+		this.name=name;
+		this.id=id;
+		this.score=score;
+	}
+
+}
---
+++ src/com/java2/object/Teacher.java
@@ -0,0 +1,13 @@
+package com.java2.object;
+
+public class Teacher extends Student {
+    int attendance;
+    int points;
+	public Teacher(String name, String id, int score) {
+		super(name, id, score);
+	}
+
+	
+	
+
+}
