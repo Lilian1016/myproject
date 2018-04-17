5e19ebc2b5bd1d2277e85b13017e65e0c4124c5b
lilian1016 <bone861016@gmail.com>
2018-04-17 16:29:16 +0800

    sogo

---
+++ src/com/java2/io/SimpleWriteTester.java
@@ -0,0 +1,13 @@
+package com.java2.io;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class SimpleWriteTester {
+	public static void main(String[] args) throws FileNotFoundException {
+				PrintStream out = new PrintStream("data.txt");
+				out.println("Hank¤j¤j\t1\t1000");
+				out.flush();
+				out.close();
+			}
+}
